1c15d6717da710b6ad94f5bd332416bd61ee8b32
wellmachado-cyber <well.machado1995@gmail.com>
2024-09-04 16:27:59 -0300

    mudei tudo aqui

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let n1 = 10
-let n2 = 30
+var prompt = require('prompt-sync')()
+let n1 = Number(prompt("Digite um número: "))
+let n2 = Number(prompt("Digite outro número: "))
 
 const soma = n1 + n2
 
