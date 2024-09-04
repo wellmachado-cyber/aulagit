418f1cf578668b625f9c98c7dadd265adf848fdf
wellmachado-cyber <well.machado1995@gmail.com>
2024-09-04 16:39:16 -0300

    Alteração

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var prompt = require('prompt-sync')()
-let n1 = Number(prompt("Digite um número: "))
-let n2 = Number(prompt("Digite outro número: "))
+// var prompt = require('prompt-sync')()
+// let n1 = Number(prompt("Digite um número: "))
+// let n2 = Number(prompt("Digite outro número: "))
 
-const soma = n1 + n2
+// const soma = n1 + n2
 
-console.log(soma)
+// console.log(soma)
+
+var prompt = require("prompt-sync")();
+
+let n1 = Number(prompt("Digite o primeiro número: "))
+let n2 = Number(prompt("Digite o segundo número: "))
+
+console.log(`A soma deles é: ${n1+n2}`)
