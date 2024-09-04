6798ad613cc58f2f976f798849673ad356269bd2
wellmachado-cyber <well.machado1995@gmail.com>
2024-09-04 16:13:43 -0300

    master

--- aulagit.js
+++
@@ -1,9 +0,0 @@
-var prompt = require('prompt-sync')()
-let numero = Number(prompt("Digite um número: "))
-
-if(numero > 10){
-    console.log("É maior que 10!")
-}
-else{
-    console.log("É menor que 10!")
-}
